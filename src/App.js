5e28d9e871ada08e167ab4a3702f97a88ca30577
Godspower Idiakose <idiakeg@gmail.com>
2022-08-20 04:37:16 +0100

    Imported and displayed individual components. Also used context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Budget from "./components/Budget";
+import Remaining from "./components/Remaining";
+import ExpenseTotal from "./components/ExpenseTotal";
+import Search from "./components/Search";
+import ExpenseList from "./components/ExpenseList";
+import AddExpenseForm from "./components/AddExpenseForm";
+import { ContextProvider } from "./contexts/Context";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ContextProvider>
+			<div className="container">
+				<header className="header">My Budget Planner</header>
+				<section className="budget-details">
+					<Budget />
+					<Remaining />
+					<ExpenseTotal />
+				</section>
+				<section className="budget-list">
+					<h2>Expenses</h2>
+					<Search />
+					<ExpenseList />
+				</section>
+				<section className="add-expense">
+					<h2>Add Expense</h2>
+					<AddExpenseForm />
+				</section>
+			</div>
+		</ContextProvider>
+	);
 }
 
 export default App;
