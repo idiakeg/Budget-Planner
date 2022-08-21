a6c00e826036f9684c317a7f501964f3e47b2e54
Godspower Idiakose <idiakeg@gmail.com>
2022-08-21 01:40:59 +0100

    Implemented the functionality to enable the user add a new Expense

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,16 +1,62 @@
-import React from "react";
+import { nanoid } from "nanoid";
+
+import { useContext } from "react";
+import Context from "../contexts/Context";
 
 const AddExpenseForm = () => {
+	const { newExpense, setNewExpense, dispatch } = useContext(Context);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let formattedName =
+			newExpense.name.charAt(0).toUpperCase() + newExpense.name.slice(1);
+
+		const expense = {
+			id: nanoid(),
+			name: formattedName,
+			cost: newExpense.cost,
+		};
+		dispatch({ type: "ADD_EXPENSE", payload: expense });
+	};
+
 	return (
 		<div className="form-container">
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div className="name-group">
 					<label htmlFor="name">Name</label>
-					<input type="text" id="name" />
+					<input
+						name="name"
+						type="text"
+						id="name"
+						value={newExpense.name}
+						onChange={(e) =>
+							setNewExpense((prev) => {
+								return {
+									...prev,
+									name: e.target.value,
+								};
+							})
+						}
+						required
+					/>
 				</div>
 				<div className="cost-group">
 					<label htmlFor="cost">Cost</label>
-					<input type="number" id="nacostme" />
+					<input
+						name="cost"
+						type="number"
+						value={newExpense.cost}
+						id="cost"
+						onChange={(e) =>
+							setNewExpense((prev) => {
+								return {
+									...prev,
+									cost: e.target.value,
+								};
+							})
+						}
+						required
+					/>
 				</div>
 				<button type="submit">Save</button>
 			</form>
