db09b72528aa939f76984811c760d0440b832e24
Godspower Idiakose <idiakeg@gmail.com>
2022-08-25 14:54:05 +0100

    Updated the exoenselist to alert the user if their search is not found.

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -8,14 +8,20 @@ const ExpenseList = () => {
 
 	const searchParameter = searchText.toLowerCase();
 
+	const filteredExpense = expenses.filter(({ name }) =>
+		name.toLowerCase().includes(searchParameter)
+	);
+
 	return (
 		<div className="expense-list-container">
 			{expenses.length > 0 ? (
-				expenses
-					.filter(({ name }) => name.toLowerCase().includes(searchParameter))
-					.map(({ id, name, cost }) => (
+				filteredExpense.length > 0 ? (
+					filteredExpense.map(({ id, name, cost }) => (
 						<ExpenseItem id={id} name={name} cost={cost} key={id} />
 					))
+				) : (
+					<div className="default-expense-data">Expense not found</div>
+				)
 			) : (
 				<div className="default-expense-data">No expense yet</div>
 			)}
